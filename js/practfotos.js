240c2ad2ddc057274389adcdd67df251f3c54abe
conchi777meca <cuentaoutlook@gmail.com>
2025-10-30 14:19:45 +0100

    arreglos 2 30/10

--- js/practfotos.js
+++ js/practfotos.js
@@ -28,11 +28,7 @@ function crearElementoLibro(libro) {
     divLibro.appendChild(url);
     divLibro.appendChild(thumbnailUrl);
 
-    "albumId": 1,
-    "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "https://via.placeholder.com/600/92c952",
-    "thumbnailUrl":
+    
 
     // Retorna un elemento div libro
     return divLibro;
